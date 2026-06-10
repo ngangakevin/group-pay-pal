c59c794d831e323d0d4d4329ad7d320cb73de0d5
Nganga Kingori <ngangakelvin254@gmail.com>
2026-06-10 13:57:23 +0300

    vercel.json file

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       allowedHosts: [
         "9f0d-2c0f-fe38-2412-74b3-5492-c39c-b869-cfa9.ngrok-free.app",
         "pagamos.vercel.app",
+        "https://group-pay-e0m4y61z4-ngangakevins-projects.vercel.app",
       ],
     },
   },
