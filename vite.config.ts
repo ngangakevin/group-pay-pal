b0ddb9a6c7f57f507f400aca7721ffc0fe9f1054
Nganga Kingori <ngangakelvin254@gmail.com>
2026-06-16 22:56:26 +0300

    prerendered app

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,19 @@
 //     error logger plugins, and sandbox detection (port/host/strictPort).
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import react from "@vitejs/plugin-react";
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
 
 export default defineConfig({
   vite: {
-    plugins: [react()],
+    plugins: [
+    tanstackStart({
+      prerender: {
+        enabled: true, // 👈 CRITICAL: This forces TanStack to output static index.html files
+      },
+    }),
+    viteReact(), // Note: react plugin must always come AFTER tanstackStart
+  ],
     base: "/group-pay-pal/",
     server: {
       allowedHosts: [
